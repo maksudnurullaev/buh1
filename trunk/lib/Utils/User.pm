7abc9174352b8f15474a6a815a2aa31c65ff351b
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2013-04-09 11:38:41 +0000

    Create new lib/Buh1/User.pm functiuonality

--- trunk/lib/Utils/User.pm
+++ trunk/lib/Utils/User.pm
@@ -15,9 +15,6 @@ use utf8;
 use ML;
 use Utils::Languages;
 
-our $LOGIN_FORMAT = '%s (<a href="/initial/login">%s</a>)'; 
-our $LOGOUT_FORMAT = '%s (<a href="/initial/logout">%s</a>)'; 
-
 sub current{
     my $self = shift;
     if( $self ){
@@ -33,21 +30,6 @@ sub is_admin{
         return(1);
     }
     return(0);
-};
-
-sub bar{
-    my $self = shift;
-    my ($result, $name, $language) = (undef, current($self), Utils::Languages::current($self));
-    if( $name ){
-        $result = sprintf $LOGOUT_FORMAT, 
-            $name, 
-            ML::get_value('Logout', $language);
-    } else {
-        $result = sprintf $LOGIN_FORMAT, 
-            ML::get_value('Guest', $language), 
-            ML::get_value('Login', $language);
-    }
-    return (Mojo::ByteStream->new($result));
 };
 
 # END OF PACKAGE
