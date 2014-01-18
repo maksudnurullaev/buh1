9e4fb43373e32c904acff442c6980d644eb2f6ca
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-01-18 07:48:58 +0000

    From bank

--- lib/Buh1/Hr.pm
+++ lib/Buh1/Hr.pm
@@ -12,43 +12,15 @@ use Mojo::Base 'Mojolicious::Controller';
 use Utils::Hr ;
 use Data::Dumper ;
 
-sub auth{
-    my ($self,$access) = @_;
-    if( !defined($self->user_role2company) 
-		|| $self->user_role2company !~ /$access/i ){
-        #TODO $self->stash( noaccess => 1 );
-        $self->redirect_to('user/login');
-		return;
-    }
-    return(1);
-};
-
-sub validate{
-    my $self = shift;
-    my $data = { 
-        object_name => $self->param('oname'),
-        creator     => Utils::User::current($self),
-    };
-	$data->{id} = $self->param('id') if $self->param('id') !~ /new/i ;
-    $data->{description} = Utils::trim $self->param('description')
-        if Utils::trim $self->param('description');
-    if( !exists $data->{description} ){
-        $data->{error} = 1 ;
-        $self->stash('description_class' => 'error');
-        $self->stash('error' => 1);
-    }
-    return($data)
-};
-
 sub add{
     my $self = shift;
-    return if( !auth($self,'write|admin') );
+    return if( !Utils::Hr::auth($self,'write|admin') );
     my $method = $self->req->method;
     if ( $method =~ /POST/ ){
         # 1. validate
-        my $data = validate($self);
-        if( !exists($data->{error}) ){
-            #TODO INSERT NEW $data ;
+        my $data = Utils::Hr::form2data($self);
+        if( Utils::Hr::validate($self,$data) ){
+            warn 'Utils::Hr::add($self, $data);';
         }
         # 2. add object to db
     } else {
@@ -57,7 +29,7 @@ sub add{
 
 sub list{
     my $self = shift;
-    return if( !auth($self,'read|write|admin') );
+    return if( !Utils::Hr::auth($self,'read|write|admin') );
 
     my $resources = Utils::Hr::get_all_resources($self);
     $self->stash( resources => $resources );
