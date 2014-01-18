fb236ef446f3e80c446b28b87cea577ed7e11814
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-01-18 07:48:58 +0000

    From bank

--- trunk/lib/Utils/Hr.pm
+++ trunk/lib/Utils/Hr.pm
@@ -17,6 +17,44 @@ use Utils::Db;
 my ($HR_DESCRIPTOR_NAME,$HR_PERSON_NAME) = 
    ('hr descriptor',    'hr person') ;
 
+sub add{
+    my ($self,$data) = @_ ;
+
+};
+
+sub auth{
+    my ($self,$access) = @_;
+    if( !defined($self->user_role2company) 
+		|| $self->user_role2company !~ /$access/i ){
+        #TODO $self->stash( noaccess => 1 );
+        $self->redirect_to('user/login');
+		return;
+    }
+    return(1);
+};
+
+sub form2data{
+    my $self = shift;
+    my $data = { 
+        object_name => $self->param('oname'),
+        creator     => Utils::User::current($self),
+    };
+	$data->{id} = $self->param('id') if $self->param('id') !~ /new/i ;
+    $data->{description} = Utils::trim $self->param('description')
+        if Utils::trim $self->param('description');
+    return($data)
+};
+
+sub validate{
+    my ($self,$data) = @_ ;
+    if( !exists $data->{description} ){
+        $self->stash('description_class' => 'error');
+        $self->stash('error' => 1);
+        return(0);
+    }
+    return(1);
+};
+
 sub get_all_resources{
     my $self = shift;
     my $db = Utils::Db::get_client_db($self);
