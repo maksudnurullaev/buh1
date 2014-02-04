4dbd2690b596a643029326b12cde423a9d6e601f
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-02-04 13:29:29 +0000

    From bank

--- lib/Utils/Files.pm
+++ lib/Utils/Files.pm
@@ -12,31 +12,29 @@ use 5.012000;
 use strict;
 use warnings;
 use utf8;
+use Utils;
 use Data::Dumper;
 
 sub add_file4id{
-	my ($self,$new_file,$id) = @_ ;
-	my $client_id = $self->session('company id') ;
-
-	my $path      = Utils::get_root_path(get_path($self,$id));
+	my ($company_id,$id,$new_file,$file_description) = @_ ;
+	my $path      = Utils::get_root_path(get_path($company_id,$id));
 	system "mkdir -p '$path/'" if ! -d $path ;
 
-	my $path_file = "$path/" . $new_file->filename;
+	my $path_file = "$path/" . Utils::get_date_uuid() ;
 	$new_file->move_to($path_file);
+
+    set_file_content($path_file . '.name', $new_file->filename) ;
+    set_file_content($path_file . '.desc', $file_description) if $file_description ;
 };
 
 sub get_path{
-	my($self,$id) = @_ ;
-	my $client_id = $self->session('company id') ;
-	return( "db/clients/$client_id/$id" ) ;
+	my($company_id,$id) = @_ ;
+	return( "db/clients/$company_id/$id" ) ;
 };
 
 sub file_list4id{
-	my ($self,$id) = @_ ;
-	my $client_id = $self->session('company id') ;
-
-	my $path = Utils::get_root_path(get_path($self,$id));
-    warn $path ;
+	my ($company_id,$id) = @_ ;
+	my $path = Utils::get_root_path(get_path($company_id,$id));
 	if( ! -d $path ){
         system "mkdir -p '$path/'" ;
         return ;
@@ -46,35 +44,35 @@ sub file_list4id{
     opendir($dir, $path);
 	my $result = {};
     while (my $file = readdir($dir)) {
-        next if ($file =~ m/^\./) || ($file =~ /\.description$/);
-		$result->{ $file } = get_file_description("$path/$file") ;
+        next if ($file =~ m/^\./) || ($file =~ /[description|name]$/);
+		$result->{ $file } = {};
+        $result->{ $file }{name} = get_file_content("$path/$file" . '.name') ;
+        $result->{ $file }{desc} = get_file_content("$path/$file" . '.desc') ;
     }
     closedir($dir);
 	return($result);
 };
 
-sub get_file_description{
-	my $file_path = shift;
-	return(undef) if !$file_path ;
-	my $description_file = $file_path . '.description' ;
-	if( -e $description_file ){
-		return get_file_content($description_file);
-	}
-	return(undef);
+sub set_file_content{
+    my($file_path,$content) = @_ ;
+	return(undef) if !$file_path || !$content ;
+    my $fh;
+    if( open($fh, ">", $file_path) ){
+        warn  "Cannot write to $file_path: $!" if ! (print $fh $content) ;
+        warn "Cannot close $file_path: $!" if !close($fh) ;
+    } else { warn "Cannot open $file_path: $!" } ;
 };
 
 sub get_file_content{
 	my $file_path = shift;
 	return(undef) if !$file_path ;
+	my($fh,$content) = (undef,undef);
 	if( -e $file_path ){
-		my($fh,$content);
 		if( open(my $fh, "< :encoding(UTF-8)", $file_path) ){
-			$content = do { local $/; <$fh> }; 
-			close($fh);
-			return($content)
-		} else {
-			warn $! ;
-		}
+		    $content = do { local $/; <$fh> }; 
+            warn "Cannot close $file_path: $!" if !close($fh) ;
+        } else { warn "Cannot open $file_path: $!" } ;
+		return($content)
 	}
 	return(undef);
 };
