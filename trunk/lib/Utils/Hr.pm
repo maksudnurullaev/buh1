6aa9194fdeefef39046c8b1b5859748e20c4f767
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-02-06 09:38:05 +0000

    from bank

--- trunk/lib/Utils/Hr.pm
+++ trunk/lib/Utils/Hr.pm
@@ -24,17 +24,6 @@ sub del{
     $dbc->del($id);
 };
 
-sub insert_or_update{
-    my ($self,$data) = @_ ;
-	my $dbc = Utils::Db::client($self) ;
-    if( exists($data->{id}) ){
-        $dbc->update( $data );
-    } else {
-        $dbc->insert( $data );
-    }
-    $self->stash(success => 1);
-};
-
 sub auth{
     my ($self,$access) = @_;
     if( !defined($self->user_role2company) 
@@ -67,53 +56,11 @@ sub validate{
     return(1);
 };
 
-sub set_parent{
+sub get_root_objects{
     my $self = shift ;
-    my $dbc = Utils::Db::client($self);
-
-    my $data = { 
-        object_name => $self->param('oname'), 
-        id          => $self->param('payload'), 
-        PARENT      => $self->param('parent') } ;
-    $dbc->update($data) ;
+    return(Utils::Db::cdb_get_root($self," WHERE name LIKE 'hr%' "));
 };
 
-sub get_resources{
-    my $self = shift;
-    my $dbc = Utils::Db::client($self);
-    if( !$dbc ){
-        warn "Could not connect to client's db!";
-        return(undef);
-    }
-    return $dbc->get_objects( { name => [ $HR_DESCRIPTOR_NAME, $HR_PERSON_NAME ] } ) ;
-};
-
-sub get_resources_root{
-    my $self = shift ;
-
-    my $dbc = Utils::Db::client($self);
-    my $resource_root = $dbc->get_root_parents(" WHERE name LIKE 'hr%' ") ;
-    my $result = {};
-    for my $pid (keys %{$resource_root}){
-        $result->{ $pid } = $dbc->get_parent_childs($pid,['description','CHILDREN']) ;
-    }
-    return( $result );
-};
-
-sub deploy{
-    my ($self,$id) = @_ ;
-    return if !$id ;
-    my $dbc = Utils::Db::client($self);
-    if( $dbc ){
-        my $objects = $dbc->get_objects( { id => [$id] } );
-        if( $objects && exists($objects->{$id}) ){
-            my $object = $objects->{$id};
-            for my $key (keys %{$object}){
-                $self->stash($key => $object->{$key});
-            }
-        }
-    }
-};
 
 # END OF PACKAGE
 };
