e4cebf5388bdb203d380e23b1c6be67f14db8b19
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-02-13 11:26:39 +0000

    From bank

---
+++ lib/Utils/Calculations.pm
@@ -0,0 +1,52 @@
+package Utils::Calculations; {
+
+=encoding utf8
+
+=head1 NAME
+
+    Database utilites 
+
+=cut
+
+use 5.012000;
+use strict;
+use warnings;
+use utf8;
+use Utils::Db;
+use Data::Dumper;
+
+sub form2data{
+    my $self = shift;
+    my $data = { 
+        object_name => $self->param('oname'),
+        creator     => Utils::User::current($self),
+        } ;
+	$data->{id} = $self->param('id') if $self->param('id') ;
+    $data->{description} = Utils::trim $self->param('description')
+        if Utils::trim $self->param('description');
+    return($data)
+};
+
+sub validate{
+    my ($self,$data) = @_ ;
+    if( !exists $data->{description} ){
+        $self->stash('description_class' => 'error');
+        $self->stash('error' => 1);
+        return(0);
+    }
+    return(1);
+};
+
+
+# END OF PACKAGE
+};
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+ M.Nurullaev <maksud.nurullaev@gmail.com>
+
+=cut
