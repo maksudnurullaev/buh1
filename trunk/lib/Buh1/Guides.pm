b86eed10d57a6eceb4eb7c3ada69934edba8e6fa
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-03-28 13:58:36 +0000

    From Bank

---
+++ trunk/lib/Buh1/Guides.pm
@@ -0,0 +1,33 @@
+package Buh1::Guides; {
+
+=encoding utf8
+
+=head1 NAME
+
+    Accounts controller
+
+=cut
+
+use Mojo::Base 'Mojolicious::Controller';
+use Utils::Files ;
+use Utils ;
+use Encode qw( encode decode_utf8 );
+use Data::Dumper ;
+
+sub page{
+    my $self = shift;
+};
+
+# END OF PACKAGE
+
+};
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+    M.Nurullaev <maksud.nurullaev@gmail.com>
+
+=cut
