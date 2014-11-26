3ab09ee6e40b18ed44f76264a60198cd2e9d0037
maksud.nurullaev <maksud.nurullaev@d4aea5b0-0c17-142a-9b7d-c214ea7de5cc>
2014-11-26 12:24:54 +0000

    Minor changes

--- lib/Mojolicious/Plugin/Cacher.pm
+++ lib/Mojolicious/Plugin/Cacher.pm
@@ -9,7 +9,7 @@ package Mojolicious::Plugin::Cacher; {
 
 =head1 USAGE
 
-    Details add soon...
+    Details dd soon...
 
 =cut
 
@@ -20,18 +20,58 @@ use utf8;
 use base 'Mojolicious::Plugin';
 use File::Path qw/make_path/;
 use Carp;
+use CHI;
+use Utils::Languages;
 use Data::Dumper;
+
+my $actions;
+my $cache;
+my @cache_conf_defaults = ('driver' => 'Memory', global => 1);
 
 sub register {
     my ($self,$app, $conf) = @_;
+    
+    if ( defined $conf->{actions} ) {
+        $actions = { map { $_ => 1 } @{ $conf->{actions} } };
+    }
 
-    # define cacher directory
-    my $cache_dir = $app->home->rel_dir('_CACHE');
-    make_path($cache_dir) if ! -e $cache_dir ;
-    # ... if still not create working directory for cacher
-    croak "Could not create working directory fo Caher!" if ! -e $cache_dir;
+    #setup cache
+    if ( !$cache ) {
+        if ( defined $conf->{options} ) {
+            my $opt = $conf->{options};
+            $opt->{driver} = $self->driver if not defined $opt->{driver};
+            $cache = CHI->new(%$opt);
+        }
+        else {
+            $cache = CHI->new( @cache_conf_defaults );
+        }
+    }
 
-    # warn Dumper $conf; 
+    if ( $app->log->level eq 'debug' ) {
+        $cache->on_set_error('log');
+        $cache->on_get_error('log');
+    }
+
+    $app->hook(
+        'before_dispatch' => sub {
+            my  $c = shift ;
+            my $path = $c->tx->req->url->path;
+            return if $path !~ /\/list$/ ;
+
+            my $lang = Utils::Languages::current($c);
+            my $unique_path = "$path/$lang";
+            warn $unique_path;
+#            $app->log->debug( ref $path );
+#            if ( $cache->is_valid($path) ) {
+#                $app->log->debug("serving from cache for $path");
+#                my $data = $cache->get($path);
+#                $c->res->code( $data->{code} );
+#                $c->res->headers( $data->{headers} );
+#                $c->res->body( $data->{body} );
+#                $c->stash( 'from_cache' => 1 );
+#            }
+        }
+    );
 };
 
 };
